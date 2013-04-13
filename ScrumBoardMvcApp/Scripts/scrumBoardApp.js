e7e5c0e2ba76df99a75208ea89c4982a83ff201e
Mick Whiteside <m.whiteside@blueyonder.co.uk>
2013-04-13 18:59:53 +0100

    scroll bar experiments

--- ScrumBoardMvcApp/Scripts/scrumBoardApp.js
+++ ScrumBoardMvcApp/Scripts/scrumBoardApp.js
@@ -1,6 +1,35 @@
 ﻿/// <reference path="External/Angular/angular.js" />
 /// <reference path="Shared/global.js" />
 /// <reference path="models.js" />
+/// <reference path="External/mCustomScrollbar/jquery.mCustomScrollbar.js" />
 
 angular.module("ScrumBoardApp", ['ui.bootstrap']);
+
+angular.module("ScrumBoardApp").ready(
+    function (){
+//        $('#scrumBoard').mCustomScrollbar({
+//            autoHideScrollbar: false,
+//            horizontalScroll: false,
+//            scrollButtons: {
+//                enable: true
+//            },
+//            advanced: {
+//                autoExpandHorizontalScroll: true
+//            }
+//        });
+
+//        $('#outerScrumBoard').mCustomScrollbar({
+//            autoHideScrollbar: false,
+//            horizontalScroll: true,
+//            scrollButtons: {
+//                enable: true
+//            },
+//            advanced: {
+//                autoExpandHorizontalScroll: true
+//            }
+//        });
+
+}());
+
+
 
